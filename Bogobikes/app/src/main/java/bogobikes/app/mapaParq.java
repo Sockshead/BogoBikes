c8b12c2d8a0fb175162d51322385058242c3f30b
Sockshead <juancapoar@gmail.com>
2018-04-01 13:14:11 -0500

    Pantalla pricipal mapas con refresh y saved instance

--- Bogobikes/app/src/main/java/bogobikes/app/mapaParq.java
+++ Bogobikes/app/src/main/java/bogobikes/app/mapaParq.java
@@ -1,5 +1,8 @@
 package bogobikes.app;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -13,16 +16,24 @@ import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.List;
@@ -34,10 +45,18 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
         LocationListener {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private boolean mRequestingLocationUpdates;
+    private LocationCallback mLocationCallback;
     private FusedLocationProviderClient mFusedLocationClient;
+    private Location mCurrentLocation;
+
+    // Keys for storing activity state in the Bundle.
+    protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+    protected final static String LOCATION_KEY = "location-key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +75,22 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
                     .build();
         }
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult){
+                if (locationResult == null){
+                    return;
+                }
+                for(Location location: locationResult.getLocations()){
+                    LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                    placeMarkerOnMap(currentLoc);
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 17));
+                }
+            }
+        };
+        mRequestingLocationUpdates=false;
+        createLocationRequest();
+        updateValuesFromBundle(savedInstanceState);
     }
 
     @Override
@@ -73,16 +108,104 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
         }
         mMap.setMyLocationEnabled(true);
 
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+       mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 if (location != null) {
                     LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
-                    placeMarkeronMap(currentLoc);
+                    //placeMarkerOnMap(currentLoc);
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 17));
+                }
+                else{
+                    return;
                 }
             }
         });
+    }
+    @SuppressLint("RestrictedApi")
+    private void createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                mRequestingLocationUpdates = true;
+                startLocationUpdates();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(mapaParq.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == RESULT_OK) {
+                mRequestingLocationUpdates = true;
+                startLocationUpdates();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+            return;
+        }
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest,mLocationCallback, null);
     }
 
     @Override
@@ -99,12 +222,11 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
         }
     }
 
-    protected void placeMarkeronMap(LatLng location) {
-        MarkerOptions markerOptions = new MarkerOptions().position(location);
+    protected void placeMarkerOnMap(LatLng location) {
+        //MarkerOptions markerOptions = new MarkerOptions().position(location);
         String titleStr = this.getAddress(location);
-        markerOptions.title(titleStr);
-
-        mMap.addMarker(markerOptions);
+        //markerOptions.title(titleStr);
+        //mMap.addMarker(markerOptions);
     }
 
     private String getAddress(LatLng latLng){
@@ -122,6 +244,36 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
             }
         }catch (IOException e){
         }return addressText;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        outState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+                mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                        REQUESTING_LOCATION_UPDATES_KEY);
+            }
+            // Update the value of mCurrentLocation from the Bundle and update the UI to show the
+            // correct latitude and longitude.
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+            // Update UI to match restored state
+            updateUI();
+        }
+    }
+
+    private void updateUI(){
+        LatLng currentLoc = new LatLng(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude());
+        //placeMarkerOnMap(currentLoc);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 17));
     }
 
     @Override
@@ -147,6 +299,9 @@ public class mapaParq extends FragmentActivity implements OnMapReadyCallback,
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         this.setUpMap();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
     }
 
     @Override
